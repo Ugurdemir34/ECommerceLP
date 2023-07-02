1496ecc5e1ff4d68ffdc987212f44f04038346f6
Ugur Demir <Ugur.Demir@adesso.com.tr>
2023-07-03 02:46:20 +0300

    Payment service created

---
+++ src/Services/Payments/Payment.Domain/Aggregate/PaymentAggregates/IntegrationEvents/Events/PaymentProcessedIntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentService.IntegrationEvents.Events
+{
+    public class PaymentProcessedIntegrationEvent : IntegrationEvent
+    {
+        public DateTime OrderDate { get; set; }
+        public float TotalPrice { get; set; }
+        public string Message { get; set; }
+        public PaymentProcessedIntegrationEvent(DateTime orderDate, float totalPrice, string message)
+        {
+            OrderDate = orderDate;
+            TotalPrice = totalPrice;
+            Message = message;
+        }
+    }
+}
---
+++ src/Services/Payments/Payments.API/PaymentService/IntegrationEvents/EventHandlers/PaymentProcessedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using EventBus.Base.Abstraction;
+using PaymentService.IntegrationEvents.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentService.IntegrationEvents.EventHandlers
+{
+    public class PaymentProcessedEventHandler : IIntegrationEventHandler<PaymentProcessedIntegrationEvent>
+    {
+        private readonly IEventBus _eventBus;
+        public PaymentProcessedEventHandler(IEventBus eventBus = null)
+        {
+            _eventBus = eventBus;
+        }
+        public Task Handle(PaymentProcessedIntegrationEvent @event)
+        {
+            Console.WriteLine($"{@event.Message}");
+            var nextEvent = new PaymentCompletedIntegrationEvent
+            {
+                TotalPrice = @event.TotalPrice,
+                Message = $"Payment succeed !"
+            };
+            _eventBus.Publish(nextEvent);
+            return Task.CompletedTask;
+        }
+    }
+}
